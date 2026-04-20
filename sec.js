9a37a2a3844563c3b22fae1c8795b63bf73e351e
Nabil Nagib <nabilnagib784@gmail.com>
2026-04-20 21:14:42 +0200

    Update sec.js

--- sec.js
+++ sec.js
@@ -1,15 +1,21 @@
 const loginBtn = document.getElementById('loginBtn');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
+
+let numLogin = 0; // متغير عام يحتفظ بعدد المحاولات
+
 loginBtn.addEventListener('click', function() {
-const username = usernameInput.value;
-const password = passwordInput.value;
-let numLogin = 0;
-if (username === 'admin@gmail.com' && password === 'school ali') {
-  window.location.href = 'users/main.html';
-  } else if (numLogin == 5 || numLogin == 10 || numLogin == 15){
-  alert('لو معاك مشكله كلمنا علي : 01030891803');
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  if (username === 'admin@gmail.com' && password === 'school ali') {
+    window.location.href = 'users/main.html';
   } else {
-  alert('اسم المستخدم أو كلمة المرور غير صحيحة!');
+    numLogin++; // زيادة عدد المحاولات
+    if (numLogin === 5 || numLogin === 10 || numLogin === 15) {
+      alert('لو معاك مشكله كلمنا علي : 01030891803');
+    } else {
+      alert('اسم المستخدم أو كلمة المرور غير صحيحة!');
+    }
   }
 });
