3e95c3538fe42253a277e92a7e597c312857b8eb
Nabil Nagib <nabilnagib784@gmail.com>
2026-04-20 21:16:20 +0200

    Add files via upload

---
+++ users/java.js
@@ -0,0 +1,11 @@
+    const messages = document.querySelectorAll(".message");
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }, { threshold: 0.2 });
+
+    messages.forEach(msg => observer.observe(msg));
