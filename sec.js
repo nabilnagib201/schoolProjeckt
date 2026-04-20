58dfee1f30e4816bbdb96a1f0d7f3ae519cfe9be
Nabil Nagib <nabilnagib784@gmail.com>
2026-04-20 21:11:28 +0200

    Enhance login validation with attempt tracking
    Added login attempt counter and alert for multiple failed attempts.

--- sec.js
+++ sec.js
@@ -4,8 +4,11 @@ const passwordInput = document.getElementById('password');
 loginBtn.addEventListener('click', function() {
 const username = usernameInput.value;
 const password = passwordInput.value;
-if (username === 'school' && password === 'ali') {
+let numLogin = 0;
+if (username === 'admin@gmail.com' && password === 'school ali') {
   window.location.href = 'users/main.html';
+  } else if (numLogin == 5 || numLogin == 10 || numLogin == 15){
+  alert('لو معاك مشكله كلمنا علي : 01030891803');
   } else {
   alert('اسم المستخدم أو كلمة المرور غير صحيحة!');
   }
